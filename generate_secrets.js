7cd3b58a35dea3da31ed8dd66192d5e80240078f
LaFouine-38 <thomasjb0208@gmail.com>
2025-07-15 21:36:57 +0200

    Update generate_secrets.js
    - retrait commentaire

--- generate_secrets.js
+++ generate_secrets.js
@@ -4,7 +4,7 @@ function generateSecret(length = 64) {
   return crypto.randomBytes(length).toString('hex');
 }
 
-const accessSecret = generateSecret(32);  // 64 hex chars
+const accessSecret = generateSecret(32);
 const refreshSecret = generateSecret(32);
 
 console.log('ACCESS_TOKEN_SECRET:', accessSecret);
